7ae7cf56b2eaa8abb052354ea656e6623dfda19f
Kunzyy <44604068+Kunzyy@users.noreply.github.com>
2018-12-07 15:53:09 +0100

    Kuntz

--- src/main/java/Main/Pizzas/CarbonaraDo.java
+++ src/main/java/Main/Pizzas/CarbonaraDo.java
@@ -7,6 +7,9 @@ public class CarbonaraDo extends Pizza{
     public CarbonaraDo()
     {
         super("Carbonara");
-
+        addIngredient(new Ingredient("pâte",3));
+        addIngredient(new Ingredient("crème blanche",(float)1.5));
+        addIngredient(new Ingredient("fromage",2));
+        addIngredient(new Ingredient("jambon",2));
     }
 }
--- src/main/java/Main/Pizzas/CarbonaraHut.java
+++ src/main/java/Main/Pizzas/CarbonaraHut.java
@@ -3,4 +3,10 @@ package Main.Pizzas;
 import Main.*;
 
 public class CarbonaraHut extends CarbonaraDo {
+
+    public CarbonaraHut()
+    {
+        super();
+        addIngredient(new Ingredient("olives",1));
+    }
 }
