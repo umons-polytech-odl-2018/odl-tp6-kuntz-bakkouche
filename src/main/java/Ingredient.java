8636d2f877ee8113457eb52f6ed71189dd5aa9d9
samybakk <samy.bakkouche@student.umons.ac.be>
2018-12-07 15:03:20 +0100

    ingrédient ok

--- src/main/java/Ingredient.java
+++ src/main/java/Ingredient.java
@@ -1,2 +1,37 @@
 public class Ingredient implements Food {
+
+
+    private String name;
+    private float price;
+
+
+
+    public Ingredient(String name, float price){
+         this.name = name;
+         this.price = price;
+     }
+
+    @Override
+     public float getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return "FOOD{" + "name ='" + name +'\'' +",price = " +price +'}';
+    }
+
 }
